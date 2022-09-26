5918b25a653f9278b17346b824bb2b879ad6a769
omer.binici <omerbinici_201@hotmail.com>
2022-09-26 17:34:11 +0300

    dosyalar yüklendi.

---
+++ classes/src/CustomerManager.java
@@ -0,0 +1,13 @@
+public class CustomerManager {
+    public void Add () {
+        System.out.println("Musteri eklendi");
+    }
+
+    public void Remove () {
+        System.out.println("Musteri silindi");
+    }
+
+    public void Uptade () {
+        System.out.println("Musteri guncellendi");
+    }
+}
